9d9df1594f32a058533d664f460107ab9bd86911
usuario06 <usuario06@172.16.97.96>
2017-08-17 10:49:30 -0600

    inicio

---
+++ src/main/java/utilities.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario06
+ */
+public class utilities {
+   
+    /**
+     * 
+     * @param texto
+     * @return 
+     * 
+     */
+    public String getResume (String texto){
+       /* texto.charAt(0);
+        texto.length();*/
+      boolean determinado; 
+       for ()
+        return texto; 
+    }
+    /**
+     * 
+     * @param texto
+     * @return 
+     */
+    public String capitalizar (String texto){
+        return texto; 
+    }
+    
+ public int contarCoincidencias (String frase, String texto){
+     int coincidencias =0; 
+     return coincidencias;  
+    }
+}
